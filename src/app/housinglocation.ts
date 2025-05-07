6e6c376bcb7f0d08227c2d95307b3d2241c9f1be
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-07 03:02:38 -0300

    feta: created interface types to housing

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,11 @@
+// Interface
+export interface Housinglocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
