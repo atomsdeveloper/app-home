335773e27191b328e1403bb6c968629eda13480b
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-07 03:06:42 -0300

    feta: created component to render only one housing  getting id params

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// Router
+import { RouterModule } from "@angular/router";
+
+// Interface
+import { Housinglocation } from "../housinglocation";
 
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: Housinglocation;
 }
+
+/*
+[src]="" -> Indica que o 'src' recebe um valor dinâmico que é 'photo' que tem a 'interface' 'HousingLocation'.
+{{}} -> Desta forma é feita a interpolação de váriaveis dinâmicas. 
+@Input() -> É a entrada dos dados dentro do componente, este component recebe dados do tipo 'HousingLocation'.
+[routerLink]="['/details', housingLocation.id] -> Link de roteamento que redireciona para '/details' passado o id do objeto atual. 
+*/
