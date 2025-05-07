52e6e9bbccd1551c36c44b344049e6ff442d7e46
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-07 03:05:28 -0300

    feat: config home components and filter results from search bar click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, inject, Inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 // Component
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
+
+// Interface
+import { Housinglocation } from "../housinglocation";
+
+// Service
+import { HousingService } from "../housing.service";
 
 @Component({
   selector: "app-home",
@@ -11,14 +17,48 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
   template: `
     <section>
       <form action="">
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: Housinglocation[] = [];
+  filteredLocationList: Housinglocation[] = [];
+  // Usando 'Service' para receber os dados necessários e renderizar o componente corretamente.
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.filteredLocationList = this.housingService.getHousingLocations();
+  }
+
+  // Função que filtra os dados de acordo com o que foi dígitado na barra de pesquisa.
+  // Passando por cada item de 'housingLocationList' e verificando a cidade que contém o texto recebido no parâmetro.
+  filterResults(text: string) {
+    if (!text) this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
+
+/*
+*ngFor= -> Diretiva para mapear uma lista/array.
+[housingLocation]= -> Passa a propriedade atual percorrida para o componente filho.
+ */
